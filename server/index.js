88ae7596845aea4deb873a82fbf26a63cfc74eb7
harshvyas235 <114279497+harshvyas235@users.noreply.github.com>
2024-05-08 19:57:17 +0530

    ch

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser()); // Parse cookies
 // Enable CORS
 app.use(
     cors({
-        origin: 'https://starter-two-ruby.vercel.app/',
+        origin: 'https://starter-two-ruby.vercel.app',
 
         
 
